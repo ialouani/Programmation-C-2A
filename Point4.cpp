d791dbe2da44df56fa3adee755d138c0b0da4346
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-07-01 09:27:42 +0100

    Encapsulation via une méthode d'initiation a la compialtion(repository deja pret) et la PROGRAMMATION SYSTEME ELEMENTAIRE(comme avant pour le shell et le make file..///

--- Point4.cpp
+++ Point4.cpp
@@ -1,7 +1,7 @@
-#include "Point4.h"
+#include "Point4.h"//cela suffit
 #include <cmath>
 #include <iostream>
-#include<string>
+#include<string>//a ajouter ces 3 derniers en cas de conflits(gestion exceptionnelle par le compilateur g++)
 using namespace std;
 void Point::setX(double x)
 {
@@ -57,3 +57,33 @@ cout << "Tapez l'abscisse : "; in >> P.x;
 cout << "Tapez l'ordonnée : "; in >> P.y;
 return in;
 }
+//implementation de cette maniere: le prototype reste le meme:
+//on enleve friend cetait juste un mot clef pour repere le lien avec non la partie publique non la partie privee de l'interface class Point dans Point4.h.
+//donc: ostream(ostrem &out, const Point &P)
+//(le contraire) ET: istream(istream &in, Point &P).
+// ajouter & operatoir << pour ostream et >> pour istream.
+//a l'interieur on va faire: prenons une instance de classe POINT::
+//et un systeme d'ecriture et de memorisation voire d'affichage avec std::count : on fait;::
+/*
+  ostream & operator<<(ostream &out, const Point P)
+on va faire normalement de l'affichage normale:
+donc on fait: 
+out(ce qui ressort) << "L'abcisse vaut" << P.x << endl;
+out << "L'ordonée vaut" << P.y << endl;
+agit discretement sur la partie privee de l'implementaition(on verra apres avec le fichier main4.cpp.
+et avec general:: friend istream & operator >>(istream &in, Point P), on aura:
+une memorisation et un affichage ### par la suite d'ou l'utilisation de count; d'ou les commandes suivantes: (suivantes de ceux qui ont ete avant)
+count << "Tapez l'abcisse: "; in >> P.x
+count << "Tapez l'ordonée: "; in >> P.y
+c'est complemtement le contraire:
+au lieu d'avoir un friend istream && operator<<(stream &out, const Point P); on aura: comme prototype d'implementation SIMPLE:(i)
+ostream && operator>>(ostream &in, Point P);
+et sur le premier suivant ce fichier lettant en oeuv re un modele concret d'interface, on aura:
+la 1er fonction au lieu de recevoir va afficher donc va faire:
+apres la 2ieme pour miuex retenir:
+count<<"Tapez l'abscisse svp"<<P.x<<endl;
+puis la deuxieme fera l'affichage du resultat compris, guide par l'utilisateur via:
+out<< "L'abcisse vait"<<((u))(); in >> P.x;
+le ostream retourne out car au contraire de recevoir il va afficher et l'autre in. 
+*/
+//inspectons maintenant le respect de main4.cpp via-a-vis de l'interface.
--- Point4.h
+++ Point4.h
@@ -1,14 +1,17 @@
 #ifndef POINT_H
 #define POINT_H
-#include<iostream>
-#include <string>
-#include<istream>
-#include<ostream>
+#include<iostream>//a inclure toujours dans un .cpp.
+#include <string>//a inclure
+#include<istream>//a inclure istream
+#include<ostream>//a inclure facile a retenir: ostream
 class Point
 {
   friend std::istream & operator>>(std::istream & out, Point &P);
+  //std:: important puis istream pour le out (moyen methechnique d'avant)
+  //std:: ostream pour le 3ieme package avec comme arguments:
+  //const Point &p et ostream &in(()) ca va s'ecrire dont reste constant ne bouge pas d'ou le const Point.
   friend std::ostream & operator<<(std:: ostream & in , const Point &P);
- 
+  //ni dans la partie publique ni dans la partie privee.
 public:
 void setX(double x);
 void setY(double y);
